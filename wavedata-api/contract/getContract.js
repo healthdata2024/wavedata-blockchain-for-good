f3e0d8b13b44d2ee5364adb76738e3aa3d59edb4
healthdata2024 <wavedata.info@gmail.com>
2024-09-24 09:10:06 +0600

    Initial commit

---
+++ wavedata-api/contract/getContract.js
@@ -0,0 +1,16 @@
+import { Abi, ContractPromise } from '@polkadot/api-contract'
+import abiData from './ink_contracts/wavedata.json';
+
+
+const address = 'Xq1gtLegXpcFibctXyJQDPSsG6XwcDRMcT132fvuaN7yfhd'//smart contract deployed address 
+	
+export default async function getContract(api) {
+
+
+    const abi = new Abi(abiData, api.registry.getChainProperties())
+
+    const contract = new ContractPromise(api, abi, address)
+
+	return contract
+  }
+  
---
+++ wavedata/src/services/getContract.js
@@ -0,0 +1,18 @@
+
+import { Abi, ContractPromise } from '@polkadot/api-contract'
+
+import abiData from '../ink_contracts/wavedata/target/ink/wavedata.json';
+
+
+const CONTRACT_ADDRESS = 'Xq1gtLegXpcFibctXyJQDPSsG6XwcDRMcT132fvuaN7yfhd'//smart contract deployed address 
+	
+export default async function getContract(api) {
+
+
+    const abi = new Abi(abiData, api.registry.getChainProperties())
+
+    const contract = new ContractPromise(api, abi, CONTRACT_ADDRESS)
+
+	return contract
+  }
+  
